baf4083c88219aa9ad6ad8e8520b32151780a1e2
bugMakker <xx114676987@qq.com>
2022-03-04 00:11:51 +0800

    add oauth2 module

---
+++ OAuth2Demo/src/main/java/com/bugMakker/OAuthApplication.java
@@ -0,0 +1,15 @@
+package com.bugMakker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auth bugYellow
+ * @datetime 2022/3/4 0:08
+ */
+@SpringBootApplication
+public class OAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthApplication.class, args);
+    }
+}
